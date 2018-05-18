692b9b416cda364696207cc5978eef8839633b9c
Grzegorz Bernas <gbernas@tmsconsultancy.local>
2018-05-18 09:35:56 +0100

    fileReader

---
+++ FileProcessor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace internishipAI
+{
+ public   class FileProcessor
+    {
+
+        public void ReadFile(string fileName)
+        {
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+            var instructions = new List<string>();
+            foreach (var line in lines)
+            {
+                if (line.Trim().ToUpper().StartsWith("G"))
+                {
+                    instructions.Add(line);
+                }
+            }
+           
+            // now calculate total lines distance by getting
+            var commandsCount = instructions.Count;
+
+            for (int i = 0; i < commandsCount-1; i++)
+            {
+                var start = instructions[i];
+                var end = instructions[i + 1];
+                var s1Commands = start.Split(" ");
+                foreach (var command in s1Commands)
+                {
+                    Console.WriteLine(command);
+                }
+
+            }
+            }
+            }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace internishipAI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileProcessor= new FileProcessor();
+            var fileName = "201_Z_Rail_Arm_Bottom_Left.gcode";
+            fileProcessor.ReadFile(fileName);
+            Console.ReadLine();
+        }
+    }
+}
